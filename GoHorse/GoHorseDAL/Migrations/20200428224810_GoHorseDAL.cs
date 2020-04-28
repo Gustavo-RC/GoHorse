edc713df35d1a83f0283e175173852670c0b91d7
Gustavo <gu171994@gmail.com>
2020-04-28 19:55:23 -0300

    Migration

---
+++ GoHorse/GoHorseDAL/Migrations/20200428224810_GoHorseDAL.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoHorseDAL.Migrations
+{
+    public partial class GoHorseDAL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
