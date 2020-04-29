ad0bd3730c2f73ac7ee1836df53420f25359934f
Gustavo <gu171994@gmail.com>
2020-04-29 02:01:57 -0300

    Classe Program Salvando e Consultando no Banco

--- GoHorse/GoHorseDAL/Program.cs
+++ GoHorse/GoHorseDAL/Program.cs
@@ -1,5 +1,6 @@
 ﻿using GoHorseClassLibrary;
 using System;
+using System.Linq;
 
 namespace GoHorseDAL
 {
@@ -7,28 +8,42 @@ namespace GoHorseDAL
     {
         static void Main(string[] args)
         {
-            // 1. Criar Objetos a serem gravados no banco
+            //// 1. Criar Objetos a serem gravados no banco
 
-            Cliente cliente = new Cliente();
-            cliente.Nome = "João";
+            //Cliente cliente = new Cliente();
+            //cliente.Nome = "João";
 
-            Animal animal = new Animal();
-            animal.Nome = "Lulu";
+            //Animal animal = new Animal();
+            //animal.Nome = "Lulu";
 
-            // 2. Estabelecer os relacionamentos
-            cliente.Animais.Add(animal);
-            animal.Dono = cliente;
+            //// 2. Estabelecer os relacionamentos
+            //cliente.Animais.Add(animal);
+            //animal.Dono = cliente;
 
-            // 3. Criar o contexto para gravar na base de dados
+            //// 3. Criar o contexto para gravar na base de dados
+
+            //GoHorseContext ctx = new GoHorseContext();
+            //ctx.Clientes.Add(cliente);
+            //ctx.Animais.Add(animal);
+            //ctx.SaveChanges();
+
+            //Console.WriteLine("Dados gravados com sucesso!");
 
             GoHorseContext ctx = new GoHorseContext();
-            ctx.Clientes.Add(cliente);
-            ctx.Animais.Add(animal);
-            ctx.SaveChanges();
+            var clientes = ctx.Clientes.ToList();
+            foreach (var cliente in clientes)
+            {
+                Console.WriteLine("Nome: " + cliente.Nome);
 
-            Console.WriteLine("Dados gravados com sucesso!");
+                var animais = from a in ctx.Animais 
+                              where a.Dono.ClienteId == cliente.ClienteId 
+                              select a;
 
-
+                foreach (var animal in animais)
+                {
+                    Console.WriteLine("Animal: " + animal.Nome);
+                }
+            }
         }
     }
 }
