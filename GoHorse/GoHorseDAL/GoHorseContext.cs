1e80e74c007899704fc748467e47b3c92e5e8786
Gustavo <gu171994@gmail.com>
2020-06-29 21:10:41 -0300

    Adicionados recursos de segurança

--- GoHorse/GoHorseDAL/GoHorseContext.cs
+++ GoHorse/GoHorseDAL/GoHorseContext.cs
@@ -1,4 +1,5 @@
 ﻿using GoHorseClassLibrary;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,8 @@ using System.Text;
 
 namespace GoHorseDAL
 {
-    public class GoHorseContext : DbContext
+    public class GoHorseContext : IdentityDbContext
+
     {
         public DbSet<Animal> Animais { get; set; }
         public DbSet<Cartao> Cartoes { get; set; }
--- GoHorse/GoHorseWeb/Startup.cs
+++ GoHorse/GoHorseWeb/Startup.cs
@@ -34,7 +34,10 @@ namespace GoHorseWeb
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
+            services.AddRazorPages()
+            .AddRazorPagesOptions(options => {
+                 options.Conventions.AuthorizeFolder("/Cadastros");
+             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
